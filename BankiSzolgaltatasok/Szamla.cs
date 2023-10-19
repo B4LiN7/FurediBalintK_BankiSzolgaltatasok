6f4ed2e7c6bbd5b8861d61cd87ebb1b0315bef65
B4LiN7 <balint.k.furedi@outlook.com>
2023-10-19 08:27:50 +0200

    3. F

---
+++ BankiSzolgaltatasok/Szamla.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankiSzolgaltatasok
+{
+    internal abstract class Szamla
+    {
+        private int 
+    }
+}
